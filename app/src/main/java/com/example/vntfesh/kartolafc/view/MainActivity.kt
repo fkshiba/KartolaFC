5641cb119ad4a3cdfda8815d43ee95936f4c62bd
vntfesh <felipe.shiba@venturus.org.br>
2017-07-03 11:32:39 -0300

    creating example fragment manager

--- app/src/main/java/com/example/vntfesh/kartolafc/view/MainActivity.kt
+++ app/src/main/java/com/example/vntfesh/kartolafc/view/MainActivity.kt
@@ -1,33 +1,44 @@
 package com.example.vntfesh.kartolafc.view
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.example.vntfesh.kartolafc.R
 import com.example.vntfesh.kartolafc.model.api.RestApi
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionListener {
+
+    private val homeFragment = HomeFragment()
+    private val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+    override fun onFragmentInteraction(uri: Uri) {
+        Log.d("Alissu", "XXX")
+    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                fragmentTransaction.replace(R.id.fragment_container, homeFragment)
+                fragmentTransaction.commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,11 +46,11 @@ class MainActivity : AppCompatActivity() {
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
-        RestApi().cartolaService.getStatus()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { status ->
-                    message.setText("Rodada atual ${status.currentRound}")
-                }
+//        RestApi().cartolaService.getStatus()
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe { status ->
+//
+//                }
     }
 }
